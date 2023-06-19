be9ddec5e4032dbdeeba5e327578a9c56e9b5e53
lLija <ilija.trokovski@gmail.com>
2023-06-20 00:45:24 +0200

    working with strings

--- C# MasterClass/Program.cs
+++ C# MasterClass/Program.cs
@@ -4,6 +4,8 @@
 
 // declaring multiple variables at once
 
+using System.Globalization;
+
 int num3, num4, num5;
 
 // declaring a variable 
@@ -55,5 +57,73 @@ Console.WriteLine("My name is " + myName);
 Console.WriteLine(message);
 Console.WriteLine(capsMessage);
 Console.WriteLine(lowerCaseMeassge);
+
+// Implicit and Explicit Conversion 
+
+// Implicit conversion
+
+int num = 123654528;
+long bigNum = num;
+
+float myFloat = 13.37F;
+double myNewDouble = myFloat;
+
+
+double MyDouble = 13.37;
+int myInt;
+
+// Explicit Conversion
+// cast double to int
+
+myInt = (int)MyDouble;
+
+// type Conversion
+
+string myString = MyDouble.ToString();
+string myFloatString = myFloat.ToString();
+
+bool sunIsShining = false;
+
+string myBoolString = sunIsShining.ToString();
+
+Console.WriteLine(myString);
+Console.WriteLine(myBoolString);
+Console.WriteLine(myFloatString);
+Console.WriteLine(myInt);
+
+// Parsing a string to an integer
+
+string myNumString = "15";
+string mySecondNumString = "13";
+int myNum1  = int.Parse(myNumString);
+int myNum2 = int.Parse(mySecondNumString);
+int result = myNum1+ myNum2;
+Console.WriteLine(result);
+
+// String Manipulation
+
+int age = 33;
+string name = "Ilija";
+string job = "Developer";
+
+// 1.String Concatenation
+Console.WriteLine("String Concatenation");
+Console.WriteLine("Hello,my name is " + name + " , I am " + age + " years old.");
+
+//2. String Formating uses index
+
+Console.WriteLine("String formating");
+Console.WriteLine("Hello, my name is {0} , I am {1} years old. I'm a {2}.",name,age,job);
+
+//3. String interpolation
+
+Console.WriteLine("String Interpolation");
+Console.WriteLine($"Hello,my name is {name}, I am {age} years old.I am {job}.");
+
+
+// Escape character
+
+string s1 = "this is a \"string\" with \na backslash \\ and a colon";
+Console.WriteLine(s1);
 
 Console.ReadLine();
